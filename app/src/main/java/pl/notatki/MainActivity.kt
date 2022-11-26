79e4aee70e16bfb7746b7bb60172d74783c60f51
Jan <janbarn00@gmail.com>
2022-11-26 21:28:56 +0100

    main-view

--- app/src/main/java/pl/notatki/MainActivity.kt
+++ app/src/main/java/pl/notatki/MainActivity.kt
@@ -7,29 +7,35 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import pl.notatki.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         //Button dodawania notatki
-        val buttonAddNote = findViewById<Button>(R.id.add_note_button) as Button
+     //   val buttonAddNote = findViewById<Button>(R.id.add_note_button) as Button
 
         //Styl dla notatki
         val notatkaStyle = ContextThemeWrapper(baseContext, android.R.style.Theme)
 
-        buttonAddNote.setOnClickListener{
-            val note_card = CardView(notatkaStyle)
-            val note_title = TextView(this)
-            note_title.text = "Title"
-            val notes_layout = findViewById<LinearLayout>(R.id.notes_layout)
-            notes_layout.addView(note_card)
-
-            note_card.addView(note_title)
+//        buttonAddNote.setOnClickListener{
+//            val note_card = CardView(notatkaStyle)
+//            val note_title = TextView(this)
+//            note_title.text = "Title"
+//            val notes_layout = findViewById<LinearLayout>(R.id.notes_layout)
+//            notes_layout.addView(note_card)
+//
+//            note_card.addView(note_title)
         }
 
 
     }
-}
