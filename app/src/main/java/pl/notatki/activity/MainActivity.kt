8d04de7625cc1a147e4c26ebcaecd8512cef1d91
Dominik Cisło <d_cislo2@o2.pl>
2022-12-22 15:58:39 +0100

    - W main_activity.xml, dodanie koloru tła do scroll view

--- app/src/main/java/pl/notatki/activity/MainActivity.kt
+++ app/src/main/java/pl/notatki/activity/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
@@ -34,16 +33,14 @@ class MainActivity : AppCompatActivity() {
         buttonAddNote.setOnClickListener {
             val notePage = Intent(this, NoteActivity::class.java)
             startActivity(notePage)
-
-
         }
+
+
         val note1 = Note(1,"Tytuł","Treść notatki",null,null,null)
         note1.title = "Tytuł"
         note1.content = "treść"
         val note2 = Note(2,"Tytuł","Treść notatki",null,null,null)
         val arrList = arrayListOf<Note>()
-
-
 
         arrList.add(note1)
         arrList.add(note2)
